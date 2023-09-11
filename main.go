1432fbb5dbe4e9257b9f730e8bcede64ea07e75e
ZondaF12 <ruaridhbell73@googlemail.com>
2023-09-11 16:42:23 +0100

    Refactored code and routes

--- main.go
+++ main.go
@@ -1,32 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ZondaF12/orders-api.git/application"
 )
 
 func main()  {
+	app := application.New()
 
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("failed to start app:", err)
 	}
-
-	err := server.ListenAndServe()
-
-	if err != nil{
-		fmt.Println("Failed to listen to server", err)
-	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("Hello World"))
 }
